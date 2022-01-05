e18342af1825a02d96f2950ec935b0e9a2673b25
Mikael Vaaltola <mikael@vaalto.la>
2022-01-05 12:34:49 +0000

    Publish CORINE landcover rasters

--- load_data.sh
+++ load_data.sh
@@ -9,3 +9,4 @@ wget https://wwwd3.ymparisto.fi/d3/gis_data/spesific/ranta10jarvet.zip && unzip 
 ogr2ogr --config PG_USE_COPY YES -f PGDump jarvi10.sql jarvi10.shp -a_srs EPSG:3067 -t_srs EPSG:4326 -lco GEOMETRY_NAME=wgs_geom -lco PRECISION=NO
 
 wget https://wwwd3.ymparisto.fi/d3/Static_rs/spesific/clc2018_fi20m.zip && unzip clc2018_fi20m.zip
+gdalwarp -te 389000 7216000 390000 7217000 Clc2018_FI20m.tif miniclc.tif
