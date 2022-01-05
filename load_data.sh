82793b87e99060c99ef1dd38b5318f8b6856b928
Mikael Vaaltola <mikael@vaalto.la>
2022-01-05 11:32:20 +0200

    Unzip correct file

--- load_data.sh
+++ load_data.sh
@@ -8,4 +8,4 @@ cd data
 wget https://wwwd3.ymparisto.fi/d3/gis_data/spesific/ranta10jarvet.zip && unzip ranta10jarvet.zip
 ogr2ogr --config PG_USE_COPY YES -f PGDump jarvi10.sql jarvi10.shp -lco SRID=3067 -lco PRECISION=NO
 
-wget https://wwwd3.ymparisto.fi/d3/Static_rs/spesific/clc2018_fi20m.zip && unzip clc2018eu25ha.zip
+wget https://wwwd3.ymparisto.fi/d3/Static_rs/spesific/clc2018_fi20m.zip && unzip clc2018_fi20m.zip
